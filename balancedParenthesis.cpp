d722807f99e38db597cd91e892f74a2af3c88387
imtiazBDSgit <imtiaz.khan.bds@gmail.com>
2021-02-08 21:12:31 +0530

    CHANGE IN BALANCED PARENTHESIS CODE ACCEPTED SOLUTION

--- balancedParenthesis.cpp
+++ balancedParenthesis.cpp
@@ -1,5 +1,5 @@
-//https://www.hackerrank.com/contests/gl-bajaj-self-evaluation-evening-batch/challenges/bracket-challenge-1/problem
 
+//https://www.hackerrank.com/contests/gl-bajaj-self-evaluation-evening-batch/challenges/bracket-challenge-1/problem
 #include<bits/stdc++.h>
 #include <cstring>
 #include <iostream>
@@ -9,36 +9,36 @@ public:
     void BP(int n,int m,vector<string> &ans,string S="",int left=0,int right=0,
     int leftb=0,int rightb=0)
         {
-             
             if(S.length()==2*n+(2*m))
-            {
-                ans.push_back(S);
-                return;
-            }
-            if(left<n)
+                {
+                    if(checkValidParenthesis(S))
+                        {
+                        ans.push_back(S);
+                        return;
+                        }
+                }
+             if(right<left)
+                {
+                        BP(n,m,ans,S+')',left,right+1,leftb,rightb);
+                } 
+             if(left<n)
                 {
                     BP(n,m,ans,S+'(',left+1,right,leftb,rightb);
                     
                 }
-            if(right<left && S[S.length()-1]!='{')
-            {
-                
-                BP(n,m,ans,S+')',left,right+1,leftb,rightb);
-            }  
-           if(leftb<m)
+                     
+            if(rightb<leftb)
                 {
-                    BP(n,m,ans,S+'{',left,right,leftb+1,rightb);
-                }
-            if(rightb<leftb && S[S.length()-1]!='(')
-            {
-                BP(n,m,ans,S+'}',left,right,leftb,rightb+1);
-                
-            }
-           
-               
-             
-            
                   
+                        BP(n,m,ans,S+'}',left,right,leftb,rightb+1);
+                    
+                }
+
+            if(leftb<m)
+                        {
+                            BP(n,m,ans,S+'{',left,right,leftb+1,rightb);
+                        }    
+ 
         }
     vector<string> generateParenthesis(int n,int m) 
     {
@@ -46,7 +46,41 @@ public:
         BP(n,m,ans);
         return ans;
     }
+    bool checkValidParenthesis(string s){
+    stack<char> st;
+     int len=s.length();
+    for(int i=0;i<len;i++){
+        if(s[i]=='{' || s[i]=='(')
+        {
+           st.push(s[i]);
+           continue;
+        }
+        else
+        {
+           if(st.top()=='(' && s[i]=='}')
+           {
+                return false;
+           }
+           else if(st.top()=='{' && s[i]==')')
+           {
+                return false;
+           }
+           else
+           {
+                st.pop();
+           }
+           
+        }
+        
+
+    }
+    if(st.empty())
+        return true;
+    else
+        return false;
+}
 };
+
 
 int main(){
     int n,m;
