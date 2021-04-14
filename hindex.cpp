a2f75e5caf929df4e009e20f1b16202614ee7089
imtiazBDSgit <imtiaz.khan.bds@gmail.com>
2021-04-14 17:27:23 +0530

    hindex

---
+++ hindex.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        int l=0;
+        int h=citations.size()-1;
+        int len=citations.size();
+        int m;
+        while(l<=h){
+            m=(l+h)/2;
+            if(citations[m]<len-m)
+                l=m+1;
+            if(citations[m]>=len-m)
+                h=m-1;
+        }
+        return len-l;
+        
+    }
+};
+
+int main(){
+    return 0;
+}
